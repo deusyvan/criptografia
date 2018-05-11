17a525f00e1dcacc4a640e349a88542de8911579
root <root@cciexinfor23.sef.eb.mil.br>
2018-05-11 12:04:37 -0300

    Reversível

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 
-//Criptografia ireversível, não consegue retornar ao valor original
+//Criptografia irreversível, não consegue retornar ao valor original
 $nome = "Deusyvan";
 $nome2 = md5($nome);
 
 echo "Nome original: ".$nome."<br/>";
 echo "Nome cripto: ".$nome2."<br/>";
 
+//Criptografia reversível, consegue retornar ao valor original
+$nome = "Deusyvan";
+$nome2 = base64_encode($nome);
 
+echo "Nome original: ".$nome."<br/>";
+echo "Nome cripto: ".$nome2."<br/>";
 ?>
